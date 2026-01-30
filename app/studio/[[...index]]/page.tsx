3d29afa587909893374b343334afaa386bf231d4
niyellll <nathaniel.krisnahadi@ti.ukdw.ac.id>
2026-01-31 01:28:36 +0700

    Remove Sanity Studio + cleanup dependencies

--- app/studio/[[...index]]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamicImport from "next/dynamic";
-
-const Studio = dynamicImport(() => import("./Studio"), { ssr: false });
-
-export default function StudioPage() {
-  return <Studio />;
-}
