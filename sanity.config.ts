5a8d12404ab97813ea7b15e68da00b097315080b
niyellll <143603121+niyellll@users.noreply.github.com>
2026-01-30 12:38:42 +0700

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,1 +1,15 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { schemaTypes } from "./sanity/schemaTypes";
+
+export default defineConfig({
+  name: "default",
+  title: "IDECN CMS",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "missing",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-01-01",
+  basePath: "/studio",
+  plugins: [deskTool()],
+  schema: { types: schemaTypes },
+});
 
